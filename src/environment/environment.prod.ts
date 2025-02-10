c7f237af47c4b1ca429ac47738e45c2066801d0c
vivekhbti <52618602+vivekhbti@users.noreply.github.com>
2025-02-11 00:13:14 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://banking-portal-api-ocp.vivekhbti-dev.svc.cluster.local:8180/api/api',
+  apiUrl: 'http://localhost:8080/api',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
